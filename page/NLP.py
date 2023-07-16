ca2e1de382a7dc5a4160deb9c1f69b9c0952bc8e
Antonio Tannoury <antonio.e.tannoury@gmail.com>
2023-07-16 04:20:58 +0300

    wordclouds

--- page/NLP.py
+++ page/NLP.py
@@ -1,11 +1,82 @@
 import streamlit as st
-from viz import image_show
+from viz import (
+    ad_grid,
+    unique_words,
+    generate_word_cloud,
+    generate_word_cloud_2020,
+    generate_word_cloud_2021,
+    generate_word_cloud_2022,
+    generate_word_cloud_2023,
+    df)
 
 
 def write():
 
     st.markdown(
         """
-        # Coming Soon ...😁
+        # Cloud 9 ☁️☁️☁️
         """
     )
+        
+    uniques = unique_words(df)
+    names = df.author.unique()
+    years = df.timestamp.dt.year.unique()
+    if "start" not in st.session_state:
+        st.session_state['years_option'] = 1
+    if 'names_option' not in st.session_state:
+        st.session_state['names_option'] = list(names)
+    if 'years_option' not in st.session_state:
+        st.session_state['years_option'] = years
+    if 'words' not in st.session_state:
+        st.session_state['words'] = set()
+
+    
+    col11, col12 = st.columns(2)
+    with col11:
+        words_to_remove = st.multiselect("Select words to remove:", uniques, key=0)
+        submit_button = st.button("Submit Words")
+        st.session_state['words'] = st.session_state['words'].union(set(words_to_remove))
+
+    col21, col22, col23, col24 = st.columns(4)
+    if submit_button or st.session_state['years_option']==1:
+        st.session_state['years_option'] = 2
+        with col21:
+            image21 = generate_word_cloud_2020(df=df, words_to_remove=st.session_state['words'])['image']
+            st.image(image21, use_column_width=True)
+        with col22:
+            image22 = generate_word_cloud_2021(df=df, words_to_remove=st.session_state['words'])['image']
+            st.image(image22, use_column_width=True)
+        with col23:
+            image23 = generate_word_cloud_2022(df=df, words_to_remove=st.session_state['words'])['image']
+            st.image(image23, use_column_width=True)
+        with col24:
+            image24 = generate_word_cloud_2023(df=df, words_to_remove=st.session_state['words'])['image']
+            st.image(image24, use_column_width=True)
+
+
+
+        
+    
+    col31, col32 = st.columns(2)
+
+    with col31:
+        names_option = st.multiselect("Select Author:", names, default=names, key=2)
+        
+    with col32:
+        years_option = st.multiselect("Select Year:", years, default=years, key=3)
+
+    if (st.session_state['years_option'] == 2) or (submit_button) or (set(st.session_state['years_option']) != set(years_option)) or (set(st.session_state['names_option']) != set(names_option)):
+    
+        col41, col42 = st.columns([3,1])
+
+        with col41:
+            image = generate_word_cloud(df=df, author=names_option, year=years_option, words_to_remove=st.session_state['words'])
+            st.session_state['years_option'] = years_option
+            st.session_state['names_option'] = names_option
+            st.session_state['years_option'] = 3
+            st.image(image['image'], use_column_width=True)            
+        with col42:
+            data = image['counts']
+            ad_grid(data,height=555)
+
+
--- viz.py
+++ viz.py
@@ -10,6 +10,11 @@ import streamlit as st
 from os import listdir
 from os.path import isfile, join
 import random
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import re
+from PIL import Image
+import numpy as np
 
 def load_data():
     df = pd.read_csv("data.csv")
@@ -182,6 +187,7 @@ at_mean_words_per_day = data_params["at_mean_words_per_day"]
 ph_mean_words_per_day = data_params["ph_mean_words_per_day"]
 metrics_dict = data_params["metrics_dict"]
 
+#%%
 def text_format(color, name, header):
     title = f"<{header} style='text-align: center; color: {color};'>{name}</{header}>"
     return title
@@ -201,7 +207,7 @@ def filter_df(
     return df_filtered
 
 
-def ad_grid(data):
+def ad_grid(data, height = 400):
     gb = GridOptionsBuilder.from_dataframe(data)
     gb.configure_pagination(paginationAutoPageSize=True, enabled=True)  # Add pagination
     gb.configure_side_bar()  # Add a sidebar
@@ -214,7 +220,7 @@ def ad_grid(data):
         fit_columns_on_grid_load=True,
         theme="streamlit", # Add theme color to the table
         enable_enterprise_modules=True,
-        height=400,
+        height=height,
         width="100%",
         reload_data=True,
     )
@@ -400,6 +406,7 @@ def image_show(height=500):
     fig.update_yaxes(showticklabels=False)
     return fig
 
+
 def metrics_df_style():
     metrics_dict = {
         'ㅤ': ["Perlei", "Love", "Antonio"], 
@@ -452,3 +459,200 @@ def metrics_df_style():
     df = df.set_table_styles(styles).to_html()
 
     return df
+
+
+stop_words = {
+        "media omited",
+        "omited",
+        "media",
+        "ba",
+        # "ana",
+        # "eh",
+        # "la",
+        # "ma",
+        # "bl",
+        # "ba",
+        # "enno",
+        # "bass",
+        # "bas",
+        # 'li',
+        # "aam",
+        # "aw",
+        # "chi",
+        # "bi",
+        # "fi",
+        # "ss",
+        # "s ",
+        # "h",
+        # "eno",
+        # "eza",
+        # "3am",
+        # "chou",
+        # "shi",
+        # "mesh",
+    }
+
+def reduce_consecutive_letters(text):
+    # Use regex to replace consecutive letters with a single letter
+    reduced_text = re.sub(r'(.)\1+', r'\1', text)
+    return reduced_text
+
+def remove_emoji(text):
+
+    emoji_pattern = re.compile("["
+            u"\U0001F600-\U0001F64F"  # emoticons
+            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                            "]+", flags=re.UNICODE)
+    return reduce_consecutive_letters(emoji_pattern.sub(r'', re.sub(r'[^\w]', ' ', text)))
+
+def remove_single_character_words(text):
+    # Define the pattern to match single character words
+    pattern = r'\b\w{1}\b'
+
+    # Remove single character words from the text
+    text_without_single_char_words = re.sub(pattern, ' ', text)
+
+    return text_without_single_char_words
+
+
+def remove_words_from_string(text, words_to_remove):
+    # Construct the regex pattern
+    pattern = r'\b(?:{})\b'.format('|'.join(map(re.escape, words_to_remove)))
+
+    # Remove the words from the string
+    modified_text = re.sub(pattern, ' ', text)
+
+    return modified_text
+
+@st.cache_data
+def generate_word_cloud_2020( df, words_to_remove = {""}, author=['Antonio','Perlei']):
+    words_to_remove = words_to_remove.union(stop_words)
+    df_filter = df.loc[(df.timestamp.dt.year.isin([2020])) & (df.author.isin(author))]
+    text = " ".join(df_filter.message.dropna().to_list()).lower()
+    text = remove_single_character_words(text)
+    text = remove_emoji(text)
+    text = remove_words_from_string(text, words_to_remove)
+    counts = pd.Series(text.split()).value_counts()
+    counts = pd.DataFrame(counts, columns=['Count'])
+# Create WordCloud object
+    mypath = f"masks/2020.jpg"
+    mask = np.array(Image.open(mypath))
+    wordcloud = WordCloud(
+        width=1200,
+        height=800,
+        background_color='white',
+        contour_width=0.0,
+        mask=mask).generate(text)
+
+      # Convert word cloud to an image using PIL
+    image = Image.fromarray(np.array(wordcloud))
+    return {"image":image, "counts":counts}
+
+@st.cache_data
+def generate_word_cloud_2021( df, words_to_remove = [""], author=['Antonio','Perlei']):
+    words_to_remove = words_to_remove.union(stop_words)
+    df_filter = df.loc[(df.timestamp.dt.year.isin([2021])) & (df.author.isin(author))]
+    text = " ".join(df_filter.message.dropna().to_list()).lower()
+    text = remove_single_character_words(text)
+    text = remove_emoji(text)
+    text = remove_words_from_string(text, words_to_remove)
+    counts = pd.Series(text.split()).value_counts()
+    counts = pd.DataFrame(counts, columns=['Count'])
+
+    mypath = f"masks/2021.jpg"
+    mask = np.array(Image.open(mypath))
+    wordcloud = WordCloud(
+        width=1200,
+        height=800,
+        background_color='white',
+        contour_width=0.0,
+        mask=mask).generate(text)
+
+      # Convert word cloud to an image using PIL
+    image = Image.fromarray(np.array(wordcloud))
+    return {"image":image, "counts":counts}
+
+@st.cache_data
+def generate_word_cloud_2022( df, words_to_remove = [""], author=['Antonio','Perlei']):
+    words_to_remove = words_to_remove.union(stop_words)
+    df_filter = df.loc[(df.timestamp.dt.year.isin([2022])) & (df.author.isin(author))]
+    text = " ".join(df_filter.message.dropna().to_list()).lower()
+    text = remove_single_character_words(text)
+    text = remove_emoji(text)
+    text = remove_words_from_string(text, words_to_remove)
+    counts = pd.Series(text.split()).value_counts()
+    counts = pd.DataFrame(counts, columns=['Count'])
+
+    mypath = f"masks/2022.jpg"
+    mask = np.array(Image.open(mypath))
+    wordcloud = WordCloud(
+        width=1200,
+        height=800,
+        background_color='white',
+        contour_width=0.0,
+        mask=mask).generate(text)
+
+      # Convert word cloud to an image using PIL
+    image = Image.fromarray(np.array(wordcloud))
+    return {"image":image, "counts":counts}
+
+@st.cache_data
+def generate_word_cloud_2023( df, words_to_remove = [""], author=['Antonio','Perlei']):
+    words_to_remove = words_to_remove.union(stop_words)
+    df_filter = df.loc[(df.timestamp.dt.year.isin([2023])) & (df.author.isin(author))]
+    text = " ".join(df_filter.message.dropna().to_list()).lower()
+    text = remove_single_character_words(text)
+    text = remove_emoji(text)
+    text = remove_words_from_string(text, words_to_remove)
+    counts = pd.Series(text.split()).value_counts()
+    counts = pd.DataFrame(counts, columns=['Count'])
+
+    mypath = "masks/2023.jpg"
+    mask = np.array(Image.open(mypath))
+    wordcloud = WordCloud(
+        width=1200,
+        height=800,
+        background_color='white',
+        contour_width=0.0,
+        mask=mask).generate(text)
+
+      # Convert word cloud to an image using PIL
+    image = Image.fromarray(np.array(wordcloud))
+    return {"image":image, "counts":counts}
+
+@st.cache_data
+def generate_word_cloud( df, words_to_remove = [""], author=['Antonio','Perlei'], year=[2020,2021,2022,2023,2024]):
+    words_to_remove = words_to_remove.union(stop_words)
+    df_filter = df.loc[(df.timestamp.dt.year.isin(year)) & (df.author.isin(author))]
+    text = " ".join(df_filter.message.dropna().to_list()).lower()
+    text = remove_single_character_words(text)
+    text = remove_emoji(text)
+    text = remove_words_from_string(text, words_to_remove)
+    counts = pd.Series(text.split()).value_counts()
+    counts = pd.DataFrame(counts, columns=['Count']).reset_index()
+    counts.columns = ['Word','Count']
+
+    onlyfiles = ["masks/chaos.jpg","masks/love.jpg","masks/cloud9.jpg","masks/heart.jpg"]
+    random_path = random.choice(onlyfiles)
+    mask = np.array(Image.open(random_path))
+
+    wordcloud = WordCloud(
+        width=1200,
+        height=800,
+        background_color='white',
+        contour_width=0.0,
+        mask=mask).generate(text)
+
+      # Convert word cloud to an image using PIL
+    image = Image.fromarray(np.array(wordcloud))
+
+    return {"image":image, "counts":counts}
+
+@st.cache_data
+def unique_words(df):
+    text = " ".join(df.message.dropna().to_list()).lower()
+    text = remove_emoji(text)
+    words = set(text.split())
+    return words
